2ab0ecbf2d1507a29dd0dbeebf95a5f94243a092
maxarvid <max.arvid.anderson@gmail.com>
2022-09-07 17:44:32 +0200

    💄 centers canvas

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -5,7 +5,12 @@
 <script setup></script>
 
 <style scoped>
-  canvas{
-    width: 800px;;
-  }
+canvas {
+  padding-left: 0;
+  padding-right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  width: 800px;
+}
 </style>
